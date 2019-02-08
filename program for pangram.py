72fd77506bf09f1a1e5b00860e205f0c5bf79f20
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2019-02-08 14:08:14 +0530

    Update and rename 10)WAP for pangram.py to program for pangram.py

---
+++ program for pangram.py
@@ -0,0 +1,10 @@
+#panagram code
+x=input("enter a string")
+x.lower()
+y=set(x)
+p={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','?'}
+if (y==p):
+     print("yes its pangram")
+else:
+    print("no pangram") 
+    
